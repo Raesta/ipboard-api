dd5cc635b3f0d91e723705126fe0f9b074513bf5
Raesta <dvs75019@gmail.com>
2016-06-06 19:42:07 +0200

    Add hello request

--- libs/core.js
+++ libs/core.js
@@ -1,9 +1,13 @@
+var utils = require('./utils');
+
 function Core(options) {
   this.options = options;
 }
 
-Core.prototype.hello = function() {
-
+Core.prototype.hello = function(callback) {
+  utils.request('GET', this.options.apiUrl + 'core/hello', this.options.apiKey, function(data) {
+    return callback(data);
+  });
 }
 
 module.exports = Core;
