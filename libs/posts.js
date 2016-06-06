4cc7960b23ff580ae29028cd2bca19c83b8362bd
Raesta <dvs75019@gmail.com>
2016-06-06 20:05:23 +0200

    Add getAll & getOne methods for posts requests

--- libs/posts.js
+++ libs/posts.js
@@ -1,5 +1,19 @@
+var utils = require('./utils');
+
 function Posts(options) {
   this.options = options;
+}
+
+Posts.prototype.getAll = function(callback) {
+  utils.request('GET', this.options.apiUrl + 'forums/posts', this.options.apiKey, function(data) {
+    return callback(data);
+  });
+}
+
+Posts.prototype.getOne = function(id, callback) {
+  utils.request('GET', this.options.apiUrl + 'forums/posts/' + id, this.options.apiKey, function(data) {
+    return callback(data);
+  });
 }
 
 module.exports = Posts;
