94cca30138020a05643148bdaba1d2932ee430b6
Raesta <dvs75019@gmail.com>
2016-06-06 20:04:15 +0200

    Add getAll & getOne methods for topics requests

--- libs/topics.js
+++ libs/topics.js
@@ -1,5 +1,19 @@
+var utils = require('./utils');
+
 function Topics(options) {
   this.options = options;
+}
+
+Topics.prototype.getAll = function(callback) {
+  utils.request('GET', this.options.apiUrl + 'forums/topics', this.options.apiKey, function(data) {
+    return callback(data);
+  });
+}
+
+Topics.prototype.getOne = function(id, callback) {
+  utils.request('GET', this.options.apiUrl + 'forums/topics/' + id, this.options.apiKey, function(data) {
+    return callback(data);
+  });
 }
 
 module.exports = Topics;
